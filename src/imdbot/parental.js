f6e0d3ac70bb7f51d4d3df05f3202c7de21dc82f
Joel McMurray <joel.mcmurray@gmail.com>
2016-09-17 02:41:02 -0700

    Some ratings have an optional closing paren we should ignore

--- src/imdbot/parental.js
+++ src/imdbot/parental.js
@@ -6,7 +6,7 @@ function getParentalGuide(url, cb) {
   x(url, {
     drugs: 'p[id="swiki.2.4.1"]',
   })((err, data) => {
-    const parental = /(\d+\/10)(.*)/g.exec(data.drugs);
+    const parental = /(\d+\/10)\)?(.*)/g.exec(data.drugs);
     cb(err, {
       rating: parental[1],
       summary: parental[2].replace(/\.(\S)/g, '. $1'),
