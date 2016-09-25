a97bcd0d2236f753d421dd8f9963fd2cac341c0a
Joel McMurray <joel.mcmurray@gmail.com>
2016-09-25 04:06:38 -0700

    Adds MXC to omdb mappings

--- src/data/titles.js
+++ src/data/titles.js
@@ -2,6 +2,7 @@ import _ from 'lodash';
 
 const mappings = {
   'The Whitest Kids U\' Know': 'The Whitest Kids U\'Know',
+  MXC: 'Most Extreme Elimination Challenge',
 };
 
 const getTitle = title => _.get(mappings, title, title);
